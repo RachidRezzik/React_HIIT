5584f5023ae5db328481e5a92bb5118bc2921b40
Rachid Rezzik <rachid.rezzik@hotmail.com>
2020-10-27 14:46:35 -0500

    Updates

---
+++ precache-manifest.987ee95b7902efc69e91f3a53a51fc4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78f6d75654ab557a3a6bd6d00b1401f",
+    "url": "/React_HIIT/index.html"
+  },
+  {
+    "revision": "179bb60c253f7a61ea37",
+    "url": "/React_HIIT/static/css/main.c8934db5.chunk.css"
+  },
+  {
+    "revision": "bb132b8a71167c8d5719",
+    "url": "/React_HIIT/static/js/2.7346dcda.chunk.js"
+  },
+  {
+    "revision": "44333f6b23a96e4dcf27b77b002c7513",
+    "url": "/React_HIIT/static/js/2.7346dcda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179bb60c253f7a61ea37",
+    "url": "/React_HIIT/static/js/main.d79cd253.chunk.js"
+  },
+  {
+    "revision": "6c22f4742dae8ac2e648",
+    "url": "/React_HIIT/static/js/runtime-main.86d64ac0.js"
+  },
+  {
+    "revision": "fbed6af8eadeb538ddedd1538d39d9b8",
+    "url": "/React_HIIT/static/media/boxingBell.fbed6af8.mp3"
+  },
+  {
+    "revision": "3e4af4627bcd9b490e19ea0c5081b5ce",
+    "url": "/React_HIIT/static/media/champions.3e4af462.mp3"
+  }
+]);
